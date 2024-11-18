641daa8f32e0c5a1aadcbde3c0d31fa0171e4dfc
Marceldgr4 <59396818+marceldgr4@users.noreply.github.com>
2024-11-18 16:32:42 -0500

    Update

--- src/components/Search/SearchPokemon.jsx
+++ src/components/Search/SearchPokemon.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
-import "./moduloSearchPokemon.css"; // Asegúrate de que el CSS está correctamente importado
+import "./moduloSearchPokemon.css";
 
 function SearchPokemon({ search, setSearch, updatePageNumber }) {
   const [input, setInput] = useState(search); 
-  const [isFocused, setIsFocused] = useState(false); // Para manejar el enfoque (clic en el input)
-  const [isTyping, setIsTyping] = useState(false); // Para manejar cuando se empieza a escribir
+  const [isFocused, setIsFocused] = useState(false); // Para manejar el enfoque
+  const [isTyping, setIsTyping] = useState(false); // Para manejar cuando se escribe
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setSearch(input); // Actualiza el estado de búsqueda
-      updatePageNumber(1); // Vuelve a la primera página después de buscar
+      updatePageNumber(1); // Vuelve a la primera página
     }, 500);
 
-    if (input.length > 0) {
-      setIsTyping(true); // Empieza a escribir
+    if (input.trim().length > 0) {
+      setIsTyping(true); // Cambia a "escribiendo"
     } else {
-      setIsTyping(false); // Deja de escribir
+      setIsTyping(false); // Vuelve al estado inicial
     }
 
     return () => clearTimeout(timeoutId);
@@ -29,7 +29,7 @@ function SearchPokemon({ search, setSearch, updatePageNumber }) {
           <img
             src="././img/search.png"
             alt="Buscar"
-            className={`search-icon ${isFocused ? 'focused' : ''}`} // Aplica clase de estilo cuando está enfocado
+            className={`search-icon ${isFocused ? "focused" : ""}`}
           />
           <input
             type="text"
@@ -37,20 +37,20 @@ function SearchPokemon({ search, setSearch, updatePageNumber }) {
             className="input-search"
             value={input}
             onChange={(e) => setInput(e.target.value)} 
-            onFocus={() => setIsFocused(true)} // Activar estado de enfoque
-            onBlur={() => setIsFocused(false)} // Desactivar estado de enfoque
+            onFocus={() => setIsFocused(true)} 
+            onBlur={() => setIsFocused(false)} 
           />
         </div>
       </section>
 
-      {/* Imagen fuera de la barra de búsqueda */}
-      {isTyping && isFocused && (
-        <img 
-          src="././img/Pokeball-9.png" 
-          alt="Imagen fuera de búsqueda" 
-          className="outside-image" 
+      {/* Imagen debajo de la barra de búsqueda */}
+      <div className="image-container">
+        <img
+          src={isTyping ? "././img/Pokeball-2.png" : "././img/Pokeball-1.png"}
+          alt="Pokeball"
+          className="pokeball-image"
         />
-      )}
+      </div>
     </div>
   );
 }
